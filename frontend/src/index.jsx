cbbdec3b777b16cdc6e0dee2851afd930e996e0c
Victor Rodrigues <victortavi@outlook.com>
2023-04-12 19:41:02 -0300

    fix: test PR

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-//alteração
+//alteração2
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
