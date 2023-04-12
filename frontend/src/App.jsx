fc2acc507501ae53644fb8c52fab7d1953c41de7
jéssica <jessy.ard11@gmail.com>
2023-04-12 19:14:52 -0300

    teste

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <Section />
       <Card />
+      {/* //teste */}
       <Footer />
     </div>
   );
